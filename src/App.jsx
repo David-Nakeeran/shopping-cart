c6fef6da433a562b053c21dc6f5f80e4ff772d9e
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-03-28 10:15:34 +0000

    Updated paths to pages.

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,30 @@
-
+import {createBrowserRouter, RouterProvider} from 'react-router-dom';
+import Home from './pages/Home';
+import Shop from './pages/Shop';
+import ProductDetail from './pages/ProductDetail';
+import Layout from './components/Layout';
+import Cart from './pages/Cart';
 
 function App() {
+
+  const router = createBrowserRouter([
+    {
+      element: <Layout />,
+      children: [
+        {path: '/', element: <Home />,},
+        {path: 'shop', element: <Shop />,},
+        {
+          path: 'shop/:productName/:id',
+          element: <ProductDetail />,
+        },
+        {path: 'cart', element: <Cart />, },
+      ],
+    },
+    
+  ])
   
   return (
-    <>
-      <h1>Hello World</h1>
-    </>
+    <RouterProvider router={router} />
   )
 }
 
