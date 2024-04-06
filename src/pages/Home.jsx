c9fad682ccd80c88d1340a5446ade2ba590fade8
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-04-06 16:58:39 +0100

    Added dummy text.

--- src/pages/Home.jsx
+++ src/pages/Home.jsx
@@ -2,6 +2,12 @@
 export default function Home() {
 
     return (
-        <h1>Home Page</h1>
+        <main>
+            <h1>Fake Store</h1>
+            <p>Fake store, fake products, what more could you want!</p>
+            <p>Come browse our random selection of products today!</p>
+            <p>You won't be dissapointed!</p>
+        </main>
+        
     )
 }
