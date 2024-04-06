23323638f90c0a61d6d466940e5e89175c9b98de
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-04-06 16:54:19 +0100

    Displays all products added to cart state Added functionality for increment/decrement quantity of each product. As well function for calculating total price.

--- src/pages/Cart.jsx
+++ src/pages/Cart.jsx
@@ -1,7 +1,92 @@
-
+import { useContext } from "react";
+import { AppContext } from "../App";
 
 export default function Cart() {
+    const {cart, setCart} = useContext(AppContext);
+
+    function incrementQuantity(productId) {
+        setCart(prevCart => {
+            return prevCart.map(item => {
+                if(item.id === productId) {
+                    return {...item, quantity: item.quantity + 1}
+                } else {
+                    return item
+                };
+            });
+        });
+    };
+
+    function decrementQuantity(productId) {
+        setCart(prevCart => {
+            return prevCart.map(item => {
+                if(item.id === productId) {
+                    if(item.quantity === 1) {
+                        return item
+                    }
+                    return {...item, quantity: item.quantity - 1}
+                } else {
+                    return item
+                };
+            });
+        });
+    };
+
+    if(!cart) {
+        return <h1>Loading...</h1>
+    }
+
+    const cartElements = cart.map(item => {
+        return (
+        
+        <div className="cart-detail" key={item.id} id={item.id}>
+            <div className="cart-img-container">
+                <img src={`${item.image}`} alt={`image of ${item.name}`}></img>
+            </div>
+            <div>
+                <p>{item.title}</p>
+                <p>£{item.price.toFixed(2)}</p>
+                <div className="cart-quantity">
+                    <button onClick={() => decrementQuantity(item.id)}>-</button>
+                    <input type='number' value={item.quantity} readOnly/>
+                    <button onClick={() => incrementQuantity(item.id)}>+</button>
+                    
+                </div>
+                <button onClick={() => removeItemFromCart(item.id)}>Remove</button>
+            </div>
+            
+        </div>
+    
+    )})
+        console.log(cart);  
+    
+    const totalPrice = cart.reduce((acc, current) => {
+        return acc + (current.price * current.quantity)
+    }, 0).toFixed(2) 
+    
+    
+    function removeItemFromCart(id) {
+        setCart(prevCart => {
+            return prevCart.filter(item => {
+                if(id !== item.id) {
+                    return item
+                }
+            })
+        })
+    }
+
+  
     return (
-        <h2>Cart goes here</h2>
+        <div>
+            <div className="cart-container">
+                {cartElements}
+            </div>
+            <div className="total-price-container">
+                <p className="cart-total-price">Total Price</p>
+                <p className="cart-total-price">£{totalPrice}</p>
+            </div>
+        </div>
+        
     )
+        
+
 }
