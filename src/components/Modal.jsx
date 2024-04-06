ad506d81c854a98b1ab34b9db6a054e8f8f0227b
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-04-06 16:50:26 +0100

    Modal for confirming item has been added to cart - still to do.

--- src/components/Modal.jsx
+++ src/components/Modal.jsx
@@ -2,11 +2,11 @@ import { useContext } from "react";
 import { AppContext } from "../App";
 
 export default function Modal() {
-    const {setIsModalOpen} = useContext(AppContext);
+    const {setIsModalOpen, cart} = useContext(AppContext);
 
     return (
         <div>
-            <button onClick={() => setIsModalOpen(false)}>blah</button>
+            <button onClick={() => setIsModalOpen(false)}>X</button>
             <p>Blah blah</p>
         </div>
     )
