a6b646e8389c09bfc1beb78aaa04e577587fe6f5
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-04-06 16:45:35 +0100

    added navlinks for components. still to do - update functionality for displaying  the number of items in the cart.

--- src/components/Header.jsx
+++ src/components/Header.jsx
@@ -1,14 +1,37 @@
-import {Link} from 'react-router-dom';
+import {NavLink, Link} from 'react-router-dom';
+import { useContext } from 'react';
+import { AppContext } from '../App';
+import loginImg from '../../public/assets/loginImg.svg';
+
+
+const activeLinkStyle = (isActive) => {
+    return isActive ? 'active-link' : null
+}
 
 export default function Header() {
+    const {cart} = useContext(AppContext);
+    
+
+
     return (
         <header>
             <nav>
                 <div>
-                <Link to='/'>Home</Link>
+                    <NavLink to='.'
+                    end
+                    className={({isActive}) => activeLinkStyle(isActive)}>Home</NavLink>
                 </div>
-                <Link to='/shop'>Shop</Link>
-                <Link to='/cart'>Cart</Link>
+                <div className='nav-links'>
+                    <NavLink to='/shop' 
+                    className={({isActive}) => activeLinkStyle(isActive)}>Shop</NavLink>
+                    <NavLink to='/cart' 
+                    className={({isActive}) => activeLinkStyle(isActive)}>{`Cart ${cart && cart.length}`}</NavLink>
+                    <Link to='/login'>
+                        <img src={loginImg}
+                        className='login-icon'/>
+                        </Link>
+                         
+                </div> 
             </nav>
         </header>
     )
