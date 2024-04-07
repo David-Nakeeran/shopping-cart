bd6c2b70c38b8ee2c469c260b2ac3451cbed6e44
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-04-07 12:12:53 +0100

    Removed useEffect and using loader for api call

--- src/pages/ProductDetail.jsx
+++ src/pages/ProductDetail.jsx
@@ -1,15 +1,18 @@
-import { Link, useParams, useLocation } from "react-router-dom";
-import{useEffect, useContext} from 'react';
+import { Link, useParams, useLocation, useLoaderData } from "react-router-dom";
+import{useEffect, useContext, useState} from 'react';
 import { AppContext } from "../App";
 import Modal from "../components/Modal.jsx";
 import { useProductData } from "../hooks/useProductData.js";
 
+export function loader({params}) {
+    return useProductData(params.id);
+}
+
 
 export default function ProductDetail() {
+    const {setCart, cart, loading, setLoading, isModalOpen, setIsModalOpen} = useContext(AppContext);
     
-    const {product, setProduct, setCart, cart, loading, setLoading, isModalOpen, setIsModalOpen} = useContext(AppContext);
-    
-    const {id} = useParams();
+    const product = useLoaderData()
     const location = useLocation();
     
 
@@ -34,23 +37,6 @@ export default function ProductDetail() {
         });
     };
 
-    
-    useEffect(() => {
-        const abortController = new AbortController();
-        async function loadProduct() {
-            setLoading(true);
-            const data = await useProductData(id);
-            setProduct(data);
-            setLoading(false);
-        }
-        loadProduct();
-        return () => abortController.abort()
-    }, []);
-
-    
-    if(loading) {
-        return <h2>Loading...</h2>
-    }
 
     const search = location.state?.search || '';
     const category = location.state?.category || 'all';
