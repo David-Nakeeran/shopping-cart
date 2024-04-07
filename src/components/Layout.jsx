8841f8279b1a8677c87f030d039e0922e28a64ef
David-Nakeeran <127234789+David-Nakeeran@users.noreply.github.com>
2024-04-07 12:11:24 +0100

    Removed unnecessary code

--- src/components/Layout.jsx
+++ src/components/Layout.jsx
@@ -1,53 +1,9 @@
 import { Outlet } from "react-router-dom";
-import {useState, useEffect, useContext} from "react"
 import Header from "./Header";
 import Footer from "./Footer";
-import { AppContext } from "../App";
-
 
 export default function Layout() {
-    // const [product, setProduct] = useState([]);
-    // const [quantity, setQuantity] = useState(1);
-    // const [cart, setCart] = useState(null)
-
     
-
-    // const testCart = [
-    // { id: 1, name: "Item 1" },
-    // { id: 1, name: "Item 1" },
-    // { id: 2, name: "Item 2" },
-    // { id: 3, name: "Item 3" },
-    // { id: 2, name: "Item 2" },
-    // { id: 1, name: "Item 1" },
-    // { id: 3, name: "Item 3" },
-    // { id: 4, name: "Item 4" }
-    // ];
-
-    // const uniqueArr = testCart.reduce((acc, current) => {
-            
-    //     const duplicate = acc.some(element => element.id === current.id)
-
-    //     if(!duplicate) {
-    //         return [...acc, current]
-    //     }
-            
-    //      return acc   
-        
-        
-    // }, [])
-    
-    // function addToCart() {
-    //     setCart(prevState => {
-    //         if(prevState === null) {
-    //             return [product]
-    //          } else {
-    //             return [...prevState,
-    //                 product
-    //             ]
-    //         }
-            
-    //     })
-    // }
     return (
         <>
             <Header />
